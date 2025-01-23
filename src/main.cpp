2c77fe9ac7fc4012b93c85268ef8cc3c0afd580c
Aaron Wald <aaronwald@gmail.com>
2025-01-23 18:39:13 +0000

    TODO fix quill

--- src/main.cpp
+++ src/main.cpp
@@ -6,59 +6,117 @@
 
 #include <echidna/event_mgr.hpp>
 
-using eight99bushwick::piapiac::DockerMgr;
+#include "quill/Backend.h"
+#include "quill/Frontend.h"
+#include "quill/LogMacros.h"
+#include "quill/Logger.h"
+#include "quill/sinks/ConsoleSink.h"
 
-// TODO: Next epoll? and send mqtt connect to server
+using namespace coypu::event;
 
-int main()
+typedef std::function<void(void)> CBType;
+typedef quill::Logger *LogType;
+typedef coypu::event::EventManager<LogType> EventManagerType;
+
+// TODO Wrap quill for libechidna
+// TODO Connect to mqtt and parse for msg
+
+typedef struct PiapiacContextS
 {
-	DockerMgr dm;
-	duckdb_result result;
-	idx_t row_count = 0;
-	idx_t column_count = 0;
-
-	if (!dm.Init())
+	PiapiacContextS(LogType &consoleLogger) : _consoleLogger(consoleLogger)
 	{
+		_eventMgr = std::make_shared<EventManagerType>(consoleLogger);
+		_set_write_ws = std::bind(&EventManagerType::SetWrite, _eventMgr, std::placeholders::_1);
+	}
+	PiapiacContextS(const PiapiacContextS &other) = delete;
+	PiapiacContextS &operator=(const PiapiacContextS &other) = delete;
+
+	LogType _consoleLogger;
+
+	std::shared_ptr<EventManagerType> _eventMgr;
+	std::function<int(int)> _set_write_ws;
+
+	std::shared_ptr<EventCBManager<CBType>> _cbManager;
+
+	std::unordered_map<int, std::pair<std::string, std::string>> _krakenChannelToPairType;
+} CoypuContext;
+
+int main(int argc [[maybe_unused]], char **argv)
+{
+	if (!argv)
+		exit(EXIT_FAILURE);
+
+	quill::Backend::start();
+
+	quill::Logger *logger = quill::Frontend::create_or_get_logger(
+			"root", quill::Frontend::create_or_get_sink<quill::ConsoleSink>("sink_id_1"));
+
+	// set terminate
+	std::set_terminate([]()
+										 { std::cout << "Unhandled exception\n";
+		std::abort(); });
+
+	// Block all signals - wait til random is collected before blocking
+	SignalFDHelper::BlockAllSignals();
+	int opt;
+
+	while ((opt = getopt(argc, argv, "d")) != -1)
+	{ // for each option...
+		switch (opt)
+		{
+		case 'd':
+			logger->set_log_level(quill::LogLevel::Debug);
+			break;
+		}
+	}
+
+	auto context = std::make_shared<CoypuContext>(logger);
+	std::weak_ptr<CoypuContext> w_context = context;
+	context->_eventMgr->Init();
+
+	// BEGIN Signal - handle signals with fd
+	sigset_t mask;
+	::sigemptyset(&mask);
+	::sigaddset(&mask, SIGINT);
+	::sigaddset(&mask, SIGKILL);
+	::sigaddset(&mask, SIGQUIT);
+	int signalFD = SignalFDHelper::CreateNonBlockSignalFD(&mask);
+	if (signalFD == -1)
+	{
+		LOG_ERROR(logger, "CreateNonBlockSignalFD {}", errno);
+
 		exit(EXIT_FAILURE);
 	}
 
-	if (!dm.Query("CREATE TABLE integers(i INTEGER, j INTEGER);"))
+	bool done = false;
+	coypu::event::callback_type readSignalCB = [&done, &logger](int fd)
 	{
-		goto cleanup;
-	}
-
-	if (!dm.Query("INSERT INTO integers VALUES (3, 4), (5, 6), (7, NULL);"))
-	{
-		goto cleanup;
-	}
-
-	if (!dm.Query("SELECT * FROM integers;", &result))
-	{
-		goto cleanup;
-	}
-
-	// print the names of the result
-	row_count = duckdb_row_count(&result);
-	column_count = duckdb_column_count(&result);
-	for (size_t i = 0; i < column_count; i++)
-	{
-		printf("%s ", duckdb_column_name(&result, i));
-	}
-	printf("\n");
-	// print the data of the result
-	for (size_t row_idx = 0; row_idx < row_count; row_idx++)
-	{
-		for (size_t col_idx = 0; col_idx < column_count; col_idx++)
+		struct signalfd_siginfo signal;
+		int count = ::read(fd, &signal, sizeof(signal));
+		if (count == -1)
 		{
-			char *val = duckdb_value_varchar(&result, col_idx, row_idx);
-			printf("%s ", val);
-			duckdb_free(val);
+			LOG_ERROR(logger, "readSignalCB");
 		}
-		printf("\n");
+		done = true;
+		return 0;
+	};
+
+	if (context->_eventMgr->Register(signalFD, readSignalCB, nullptr, nullptr))
+	{
+		LOG_ERROR(logger, "Register {}, errno");
 	}
-	// duckdb_print_result(result);
+	// END signal
+
+	// wait til signal
+	while (!done)
+	{
+		if (context->_eventMgr->Wait() < 0)
+		{
+			done = true;
+		}
+	}
+
+	std::cout << "piapiac leaving" << std::endl;
+
 	exit(EXIT_SUCCESS);
-cleanup:
-	duckdb_destroy_result(&result);
-	exit(EXIT_FAILURE);
 }
