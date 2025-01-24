8a6781f8684c5e8e241323aa0fd9138e82279701
Aaron Wald <aaronwald@gmail.com>
2025-01-24 02:10:29 +0000

    latest

--- include/mqtt.hpp
+++ include/mqtt.hpp
@@ -19,40 +19,40 @@ namespace eight99bushwick
 
     enum class MqttControlPacketType : uint8_t
     {
-      RESERVED = 0,
-      CONNECT = 1,
-      CONNACK = 2,
-      PUBLISH = 3,
-      PUBACK = 4,
-      PUBREC = 5,
-      PUBREL = 6,
-      PUBCOMP = 7,
-      SUBSCRIBE = 8,
-      SUBACK = 9,
-      UNSUBSCRIBE = 10,
-      UNSUBACK = 11,
-      PINGREQ = 12,
-      PINGRESP = 13,
-      DISCONNECT = 14,
-      AUTH = 15
+      MQ_CPT_RESERVED = 0,
+      MQ_CPT_CONNECT = 1,
+      MQ_CPT_CONNACK = 2,
+      MQ_CPT_PUBLISH = 3,
+      MQ_CPT_PUBACK = 4,
+      MQ_CPT_PUBREC = 5,
+      MQ_CPT_PUBREL = 6,
+      MQ_CPT_PUBCOMP = 7,
+      MQ_CPT_SUBSCRIBE = 8,
+      MQ_CPT_SUBACK = 9,
+      MQ_CPT_UNSUBSCRIBE = 10,
+      MQ_CPT_UNSUBACK = 11,
+      MQ_CPT_PINGREQ = 12,
+      MQ_CPT_PINGRESP = 13,
+      MQ_CPT_DISCONNECT = 14,
+      MQ_CPT_AUTH = 15
     };
 
     enum class MqttFlags : uint8_t
     {
-      PUBLISH_DUP = 0x08,
-      PUBLISH_QOS2 = 0x04,
-      PUBLISH_QOS1 = 0x02,
-      PUBLISH_RETAIN = 0x01,
+      MQ_FLAG_PUBLISH_DUP = 0x08,
+      MQ_FLAG_PUBLISH_QOS2 = 0x04,
+      MQ_FLAG_PUBLISH_QOS1 = 0x02,
+      MQ_FLAG_PUBLISH_RETAIN = 0x01,
     };
 
     enum class MqttState : uint8_t
     {
-      DISCONNECTED = 0,
-      CONNECTING = 1,
-      CONNECTED = 2,
-      DISCONNECTING = 3,
-      DONE = 4,
-      ERROR = 4,
+      MQ_STATE_DISCONNECTED = 0,
+      MQ_STATE_CONNECTING = 1,
+      MQ_STATE_CONNECTED = 2,
+      MQ_STATE_DISCONNECTING = 3,
+      MQ_STATE_DONE = 4,
+      MQ_STATE_ERROR = 4,
     };
 
     // supports non blocking io
